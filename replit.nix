d0c9446a9372d1c07200d33128229645b0c07d4c
Aryan-Khanijo <akhanijo12345@gmail.com>
2025-04-01 02:11:50 +0530

    config

--- replit.nix
+++ replit.nix
@@ -1,14 +1,10 @@
 { pkgs }: {
   deps = [
-    pkgs.nodejs-16_x
+    pkgs.nodejs-18_x
     pkgs.nodePackages.typescript-language-server
     pkgs.nodePackages.yarn
-    pkgs.replitPackages.jest
-    pkgs.python38Full
+    pkgs.python3
     pkgs.ffmpeg
     pkgs.libuuid
   ];
-  env = {
-     LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.libuuid];
-  };
 }
